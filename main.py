e498d2157854b0de7b7d2198141e117b94f16fe5
Pierre Sibut-Bourde <pierre.sibut.bourde@gmail.com>
2022-03-20 16:00:45 +0100

    cleanup not clean

--- main.py
+++ main.py
@@ -4,7 +4,5 @@ import statsapi
 
 if __name__=="__main__":
     print("Welcome to MLB-FR data collector.")
-    # sample_gamePk = [706907, 706859, 706872, 706820, 706922, 707028, 707009, 707003, 706806, 706942, 706836, 706978, 706792, 706993]
-    # print(scoring.score_games(sample_gamePk))
     gamePks = [706907, 706859, 706872, 706820, 706922, 707028, 707009, 707003, 706806, 706942, 706836, 706978, 706792, 706993] # [statsapi.last_game(id) for id in g.id_of_teams]
     scoring.score_games(gamePks)
